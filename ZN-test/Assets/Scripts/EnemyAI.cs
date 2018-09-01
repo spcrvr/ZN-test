a26eb11e3aa82ee74edb47eb957f736b51a0b491
spcrvr <beni.grohar@gmail.com>
2018-09-01 22:12:03 +0200

    Added some scripts

---
+++ ZN-test/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace StatePattern
+{
+	public class EnemyAI : MonoBehaviour
+	{
+		public GameObject playerObj;
+		public GameObject walkerObj;
+		List<Enemy> enemies = new List<Enemy>();
+
+		void Start()
+		{
+			enemies.Add(new Walker(walkerObj.transform));
+
+		}
+
+		void FixedUpdate()
+		{
+			for(int i=0;i<enemies.Count;i++)
+			{
+				enemies[i].UpdateEnemy(playerObj.transform);
+			}
+
+		}
+	}
+
+
+}
