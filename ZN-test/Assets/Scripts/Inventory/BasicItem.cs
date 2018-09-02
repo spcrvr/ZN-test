c29892b8d3ae5a3677b04966580921cdda0d5931
spcrvr <beni.grohar@gmail.com>
2018-09-02 22:52:56 +0200

    Update

--- ZN-test/Assets/Scripts/Inventory/BasicItem.cs
+++ ZN-test/Assets/Scripts/Inventory/BasicItem.cs
@@ -19,4 +19,25 @@ public class BasicItem {
 		
 	}
 #endregion
+private ushort maxAmount; // number that represents the maximum amount of an item in one stack (like in Minecraft)
+public UnityEvent OnMaxAmountChange = new UnityEvent();
+public ushort MaxAmount
+{
+    get { return maxAmount; }
+    set { maxAmount = value; OnMaxAmountChange.Invoke(); }
+}
+private ushort amount; // number that represents amount of an item in one stack
+public UnityEvent OnAmountChange = new UnityEvent();
+public ushort Amount
+{
+    get { return amount; }
+    set { amount = value; OnAmountChange.Invoke(); }
+}
+private float durability; // number 0-1 that represents the durability
+public UnityEvent OnDurabilityChange = new UnityEvent();
+public float Durability
+{
+    get { return durability; }
+    set { durability = value; OnDurabilityChange.Invoke(); }
+}
 }
